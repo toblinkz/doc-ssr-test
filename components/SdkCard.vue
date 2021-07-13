37214803788712a349b739656892f6b1ceb1cbe5
Omole Priscilla <priscilla.omole@stu.cu.edu.ng>
2021-07-13 18:12:18 +0100

    feat: update page content

--- components/SdkCard.vue
+++ components/SdkCard.vue
@@ -3,7 +3,7 @@
     <div class="flex">
 
       <button class=" w-1/2 no-underline   mr-4 cursor-pointer ">
-        <div class="flex font-bold">GO SDK</div>
+
         <div class="mt-3 rounded-sm rounded-md rounded-lg overflow-hidden bg-white  hover:shadow-lg rounded-lg" style="border: 1px solid #E0E0E0">
           <div class="mx-6 my-5 ">
             <div class="flex items-center font-medium font-bold text-base text-blue-700 mb-3">Termii GO <span class="ml-3"><i class="icon-github text-black"></i></span></div>
@@ -14,7 +14,7 @@
         </div>
       </button>
       <button class=" w-1/2 no-underline   mr-4 cursor-pointer ">
-        <div class="flex font-bold">PHP SDK</div>
+
         <div class="mt-3 rounded-sm rounded-md rounded-lg overflow-hidden bg-white hover:shadow-lg rounded-lg" style="border: 1px solid #E0E0E0">
           <div class="mx-6 my-5 ">
             <div class="flex items-center font-medium font-bold text-base text-blue-700 mb-3">Termii PHP <span class="ml-3"><i  class="icon-github text-black"></i></span></div>
@@ -29,7 +29,7 @@
     <div class="mt-3 flex">
 
       <button class=" w-1/2 no-underline   mr-4 cursor-pointer ">
-        <div class="flex font-bold">Laravel SDK</div>
+
         <div class="mt-3 rounded-sm rounded-md rounded-lg overflow-hidden bg-white  hover:shadow-lg rounded-lg" style="border: 1px solid #E0E0E0">
           <div class="mx-6 my-5 ">
             <div class="flex items-center font-medium font-bold text-base text-blue-700 mb-3">Termii Laravel <span class="ml-3"><i class="icon-github text-black"></i></span></div>
@@ -41,16 +41,32 @@
       </button>
 
       <button class=" w-1/2 no-underline   mr-4 cursor-pointer ">
-        <div class="flex font-bold">PHP CLI</div>
         <div class="mt-3 rounded-sm rounded-md rounded-lg overflow-hidden bg-white hover:shadow-lg rounded-lg" style="border: 1px solid #E0E0E0">
           <div class="mx-6 my-5 ">
             <div class="flex items-center font-medium font-bold text-base text-blue-700 mb-3">Termii PHP <span class="ml-3"><i  class="icon-github text-black"></i></span></div>
             <a  href="https://github.com/Mane-Olawale/termii" target="_blank" class="flex font-normal text-gray-dark text-sm mb-2" style="text-decoration: underline!important;">
-              Mane-Olawale/termii-cli
+              Mane-Olawale/termii-php
             </a>
           </div>
         </div>
       </button>
+    </div>
+
+    <div class="mt-3 flex">
+
+      <button class=" w-1/2 no-underline   mr-4 cursor-pointer ">
+
+        <div class="mt-3 rounded-sm rounded-md rounded-lg overflow-hidden bg-white  hover:shadow-lg rounded-lg" style="border: 1px solid #E0E0E0">
+          <div class="mx-6 my-5 ">
+            <div class="flex items-center font-medium font-bold text-base text-blue-700 mb-3">Termii PHP <span class="ml-3"><i class="icon-github text-black"></i></span></div>
+            <a  href="https://github.com/zeevx/termii-cli"  target="_blank" class="flex font-normal text-gray-dark text-sm mb-2" style="text-decoration: underline!important;">
+              zeevx/termii-cli
+            </a>
+          </div>
+        </div>
+      </button>
+
+
     </div>
   </div>
 </template>
